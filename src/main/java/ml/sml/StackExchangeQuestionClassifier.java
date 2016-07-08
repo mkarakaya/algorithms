b7e098287d254496073e780a3e1abc603c1cc8f1
Karakaya <212457624@logon.ds.ge.com>
2016-07-08 17:25:49 +0300

    edit StackExchangeQuestionClassifier for 10 points

--- src/main/java/ml/sml/StackExchangeQuestionClassifier.java
+++ src/main/java/ml/sml/StackExchangeQuestionClassifier.java
@@ -20,57 +20,62 @@ import java.util.Scanner;
  * Created by 212457624 on 04.07.2016.
  */
 public class StackExchangeQuestionClassifier {
+    public static boolean VALIDATE=true;
     public static void main(String[] args) throws FileNotFoundException, ParseException {
-        URL resource = StackExchangeQuestionClassifier.class.getClassLoader().getResource("training.json");
-        Scanner in = new Scanner(new File(resource.getFile()));
-        //Scanner in = new Scanner(System.in);
-        int size = in.nextInt();
-        int count=0;
         List<Datum<String,String>> trainingData = new ArrayList<>();
         JSONParser parser = new JSONParser();
-        while(count!=size) {
-            String json = in.nextLine();
-            if(!"".equals(json)) {
-                try {
-                    List<String> features = new ArrayList<>();
-                    JSONObject jsonObj = (JSONObject) parser.parse(json);
-                    features.add("question="+jsonObj.get("question"));
-                    features.add("excerpt="+jsonObj.get("excerpt"));
-                    trainingData.add(new BasicDatum(features,jsonObj.get("topic")));
-
-
-                }catch (Exception e){
-                    System.out.println(e.getMessage());
-                }finally {
-                    count++;
-                }
-
-            }
-        }
-        Scanner cin = new Scanner(System.in);
-        int cSize = cin.nextInt();
+        URL resource = StackExchangeQuestionClassifier.class.getClassLoader().getResource("training.json");
+        Scanner cin = new Scanner(new File(resource.getFile()));
+        int cSize = Integer.parseInt(cin.nextLine().trim());
         List<BasicDatum> datums=new ArrayList<>();
-        for(int i=0;i<cSize;i++){
-            JSONObject jsonObj = (JSONObject) parser.parse(cin.next());
-            List<String> features = new ArrayList<>();
-            features.add("question="+jsonObj.get("question"));
-            features.add("excerpt="+jsonObj.get("excerpt"));
-            BasicDatum datum= new BasicDatum(features,null);
-            datums.add(datum);
-            trainingData.add(datum);
-        }
+        addTraining(trainingData, parser, cin, cSize, datums,false);
+
+        resource = StackExchangeQuestionClassifier.class.getClassLoader().getResource("test.json");
+        cin = new Scanner(new File(resource.getFile()));
+        //cin = new Scanner(System.in);
+        cSize = Integer.parseInt(cin.nextLine().trim());
+        addTraining(trainingData, parser, cin, cSize, datums,true);
 
         LinearClassifierFactory<String,String> factory = new LinearClassifierFactory<>();
         factory.useConjugateGradientAscent();
         // Turn on per-iteration convergence updates
-        factory.setVerbose(true);
+        factory.setVerbose(false);
         //Small amount of smoothing
-        factory.setSigma(10.0);
+        //factory.setSigma(10.0);
         LinearClassifier<String,String> classifier = factory.trainClassifier(trainingData);
-        for(BasicDatum datum:datums){
-            System.out.println(classifier.classOf(datum));
+        if(VALIDATE) {
+            resource = StackExchangeQuestionClassifier.class.getClassLoader().getResource("validate.txt");
+            cin = new Scanner(new File(resource.getFile()));
+            double count=0;
+            for (BasicDatum datum : datums) {
+                String result=cin.nextLine();
+                String predicted=classifier.classOf(datum);
+                if(result.equals(predicted)){
+                    count++;
+                }
+                System.out.println(result+":"+predicted);
+            }
+            System.out.println("%"+(count/datums.size())+":"+count+"/"+datums.size());
+        }else{
+            for (BasicDatum datum : datums) {
+                System.out.println(classifier.classOf(datum));
+            }
         }
 
+    }
 
+    private static void addTraining(List<Datum<String, String>> trainingData, JSONParser parser,
+                                    Scanner cin, int cSize, List<BasicDatum> datums,boolean save) throws ParseException {
+        for(int i=0;i<cSize;i++){
+            JSONObject jsonObj = (JSONObject) parser.parse(cin.nextLine());
+            List<String> features = new ArrayList<>();
+            features.add("question="+jsonObj.get("question"));
+            features.add("excerpt="+jsonObj.get("excerpt"));
+            BasicDatum datum= new BasicDatum(features,jsonObj.get("topic"));
+            if(save) {
+                datums.add(datum);
+            }
+            trainingData.add(datum);
+        }
     }
 }
