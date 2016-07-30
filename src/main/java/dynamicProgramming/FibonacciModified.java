6e3648bd51f40c9f544ca1c3c1412dacd559d7e8
Karakaya <212457624@logon.ds.ge.com>
2016-07-30 14:31:31 +0300

    add FibonacciModified

---
+++ src/main/java/dynamicProgramming/FibonacciModified.java
@@ -0,0 +1,27 @@
+package dynamicProgramming;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * Created by 212457624 on 27.07.2016.
+ */
+public class FibonacciModified {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner in = new Scanner(new File("c:/tmp/input01.txt"));
+        BigInteger first = in.nextBigInteger();
+        BigInteger second= in.nextBigInteger();
+        int n = in.nextInt();
+        fibonacci(first,second,n-1);
+    }
+
+    private static void fibonacci(BigInteger first, BigInteger second, int n) {
+        if(n==0){
+            System.out.println(first);
+        }else{
+            fibonacci(second,second.multiply(second).add(first),n-1);
+        }
+    }
+}
