08dd37ce9e2831adb1731c946b2b8cdd32fd53e3
mokarakaya <mozgkarakaya@gmail.com>
2016-10-28 22:07:41 +0300

    add concurrency

---
+++ src/main/java/javaFeatures/concurrency/CountDownLatchMain.java
@@ -0,0 +1,7 @@
+package javaFeatures.concurrency;
+
+/**
+ * Created by mokarakaya on 28.10.2016.
+ */
+public class CountDownLatchMain {
+}
--- src/main/java/javaFeatures/stream/FoldList.java
+++ src/main/java/javaFeatures/stream/FoldList.java
@@ -1,4 +1,4 @@
-package javaStream;
+package javaFeatures.stream;
 
 import java.util.Arrays;
 import java.util.List;
